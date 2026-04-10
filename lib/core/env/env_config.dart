59e5d84beb428db4bef887f02fb05c6fadbe1fe9
Jagmeet236 <jagmeetsing236@gmail.com>
2026-04-10 11:00:53 +0530

    add secure token storage and environment configuration support

--- lib/core/env/env_config.dart
+++ lib/core/env/env_config.dart
@@ -1,6 +1,17 @@
+enum Environment { staging, production }
+
 class EnvConfig {
-  static const String baseUrl = String.fromEnvironment(
-    'API_BASE_URL',
-    defaultValue: 'https://api2-staging.codemitra.org',
-  );
+  static Environment currentEnv = Environment.staging;
+
+  static String get baseUrl {
+    switch (currentEnv) {
+      case Environment.staging:
+        return 'https://api2-staging.codemitra.org';
+
+      case Environment.production:
+        return '';
+    }
+  }
+
+  static bool get isProd => currentEnv == Environment.production;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:auth_flow_app/core/di/service_locator.dart';
 import 'package:auth_flow_app/core/theme/app_theme.dart';
 import 'package:auth_flow_app/src/splash/presentation/ui/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await Hive.initFlutter();
+
   setupLocator();
   runApp(const MyApp());
 }
